0c04ede1b9a474984688b05855d1443311478c78
Alex Yang <yanglex@gmail.com>
2024-09-13 05:43:40 -0500

    Condensed Line class into function in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,39 @@
-#include "../include/line.h"
 #include "../include/olcPixelGameEngine.h"
 
 class TinyRenderer : public olc::PixelGameEngine {
 public:
-  TinyRenderer() { sAppName = "TinyRenderer"; }
+  TinyRenderer() { sAppName = "Tiny Renderer"; }
 
 public:
   bool OnUserCreate() override {
     // Called once at the start, so create things here
+    void line(int x0, int y0, int x1, int y1, olc::Pixel p);
     return true;
+  }
+
+  // Bresenham's Line Algorithm
+  void line(int x0, int y0, int x1, int y1, olc::Pixel p) {
+    int dx{x1 - x0};
+    int dy{y1 - y0};
+    int d{2 * dy - dx};
+    for (int x{x0}, y{y0}; x <= x1; x++) {
+      if (d > 0) {
+        Draw(x, y++, p);
+        // d = d - 2 * dx;
+        d = 2 * dy - 2 * dx;
+      } else {
+        Draw(x, y, p);
+        // d = d + 2 * dy;
+        d = 2 * dy;
+      }
+    }
   }
 
   bool OnUserUpdate(float fElapsedTime) override {
     // called once per frame
-    Line please(1, 1, 14, 6, olc::RED);
-    please.lineD(this);
+    line(13, 20, 80, 40, olc::WHITE);
+    line(20, 13, 40, 80, olc::RED);
+    line(80, 40, 13, 20, olc::RED);
     if (GetKey(olc::Key::ESCAPE).bPressed) {
       return false;
     }
@@ -24,7 +43,7 @@ public:
 
 int main() {
   TinyRenderer test;
-  if (test.Construct(100, 100, 4, 4))
+  if (test.Construct(100, 100, 1, 1))
     test.Start();
 
   return 0;

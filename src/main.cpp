33679e3030c1f4f823b7a03d28611e7eb72e8e9b
Alex Yang <a1ecks@Macmini.lan>
2025-07-21 02:29:49 -0500

    Cleaned up unused code

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,6 @@
 #define OLC_PGE_APPLICATION
 #include "../include/olcPixelGameEngine.h"
-// #include "../include/tgaimage.h"
 #include <cmath>
-
-// Original code provided by tinyrenderer repository
-//
-// constexpr TGAColor white = {255, 255, 255, 255}; // attention, BGRA order
-// constexpr TGAColor green = {0, 255, 0, 255};
-// constexpr TGAColor red = {0, 0, 255, 255};
-// constexpr TGAColor blue = {255, 128, 64, 255};
-// constexpr TGAColor yellow = {0, 200, 255, 255};
-//
-// void line(int ax, int ay, int bx, int by, TGAImage &framebuffer,
-//           TGAColor color) {
-//   for (float t = 0.; t < 1.; t += .02) {
-//     int x = std::round(ax + (bx - ax) * t);
-//     int y = std::round(ay + (by - ay) * t);
-//     framebuffer.set(x, y, color);
-//   }
-// }
-//
-// int main(int argc, char **argv) {
-//   constexpr int width = 64;
-//   constexpr int height = 64;
-//   TGAImage framebuffer(width, height, TGAImage::RGB);
-//
-//   int ax = 7, ay = 3;
-//   int bx = 12, by = 37;
-//   int cx = 62, cy = 53;
-// framebuffer.set(ax, ay, white); framebuffer.set(bx, by, white);
-//   framebuffer.set(cx, cy, white);
-//   line(ax, ay, bx, by, framebuffer, blue);
-//   line(cx, cy, bx, by, framebuffer, green);
-//   line(cx, cy, ax, ay, framebuffer, yellow);
-//   line(ax, ay, cx, cy, framebuffer, red);
-//
-//   framebuffer.write_tga_file("framebuffer.tga");
-//   return 0;
-// }
-//
 
 class TinyRenderer : public olc::PixelGameEngine {
 public:
@@ -56,16 +18,6 @@ public:
 
   // Adjust Y coordinate to match tinyrenderer guide
   int fixY(int y) { return std::abs(HEIGHT - y); }
-
-  // Simple implementation with floating point numbers
-  //
-  //  void line(int x0, int y0, int x1, int y1, olc::Pixel p) {
-  //    for (float m{0.0}; m < 1.0; m += 0.02) {
-  //      int x = std::round(x0 + (x1 - x0) * m);
-  //      int y = std::round(y0 + (y1 - y0) * m);
-  //      Draw(x, y, p);
-  //    }
-  //  }
 
   void line(int x0, int y0, int x1, int y1, olc::Pixel p) {
     bool v{std::abs(x1 - x0) < std::abs(y1 - y0)};
